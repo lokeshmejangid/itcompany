7061afb8588b6d0cc239263ef4a01280f3185e91
nitsoni7 <nitinni144@gmail.com>
2024-02-21 15:12:31 +0530

    set main slider

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,34 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import SliderBannerArr from '../../Utils/SliderBanner.json'
+// import SliderBannerArr from '../../Utils/SliderBanner.json'
 import SliderBanner from '../../InnerComponent/SliderBanner/SliderBanner';
+import { withNavigation } from 'react-awesome-slider/dist/navigation';
+
 
 const Home = () => {
+  // const MySliderWithNavigation = withNavigation(SliderBanner);
 
-  const [sliderCount, setSliderCount] = useState(1);
-
-  const next = () => {
-    sliderCount === SliderBannerArr.length ? setSliderCount(1) : setSliderCount(sliderCount + 1);
-  }
-
-  const previous = () => {
-    sliderCount === 1 ? setSliderCount(SliderBannerArr.length) : setSliderCount(sliderCount - 1);
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      next();
-    }, 5000);
-  });
-
-  const styles = {
-    cardBg: {
-      backgroundImage: `url(./image/SectionBackground.jpg)`
-    }
-  }
 
   return (
     <>
-      {SliderBannerArr.map((value, index) => sliderCount === index + 1 ? <SliderBanner val={value} key={index} next={next} pre={previous} /> : "")}
+     {/* <MySliderWithNavigation /> */}
+     <SliderBanner />
     </>
   )
 }
--- src/InnerComponent/SliderBanner/SliderBanner.jsx
+++ src/InnerComponent/SliderBanner/SliderBanner.jsx
@@ -1,33 +1,25 @@
-import { Grid } from '@mui/material'
 import React from 'react'
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 import './SliderBanner.css'
-import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material'
-import CusButton from './CusButton/CusButton'
 
-const SliderBanner = (props) => {
-  const { val, ind, next, pre } = props;
-  const { h2, p, img } = val;
-  const styles = {
-    banner: {
-      backgroundImage: `url(${img})`
-    }
-  };
+
+const SliderBanner = () => {
   return (
-    <Grid container spacing={0} className='Slider' justifyContent='center' style={styles.banner} >
-      <div className='black'></div>
-      <Grid container spacing={0} item sm={10} xs={11}>
-        <Grid item sm={6} xs={12}>
-          <h2>{h2}</h2>
-          <p>{p}</p>
-          <CusButton name='Book Now' />
-          <div className="errow">
-            <ArrowBackIos className='Arrow' onClick={pre} />
-            <ArrowForwardIos className='Arrow' onClick={next} />
-          </div>
-        </Grid>
-      </Grid>
-    </Grid>
-  )
+    <AwesomeSlider
+      bullets={false}
+      className='MainSlider'
+    >
+      <div data-src="./image/bannerImages/1.webp" className='Contant'>
+        <h2>Hello World</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div data-src="./image/bannerImages/2.jpg" className='Contant'>
+        <h2>Hello World</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </AwesomeSlider>
+  );
 }
 
 export default SliderBanner
